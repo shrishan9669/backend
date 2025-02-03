04716e983c3bb9828d9a147555704ef0c6af7afd
shrishan9669 <shrishan9669@gmail.com>
2025-02-03 06:59:05 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 // Refine CORS setup
 app.use(
   cors({
-    origin: 'https://eduarchiv.vercel.app',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
    allowedHeaders: ['Content-Type', 'Authorization']
   })
